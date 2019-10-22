7b7b753291db6287133e0b9aff2b52ac8cef835c
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2019-10-22 11:13:03 -0300

    correção erro

--- backend/controllers/CountryController.php
+++ backend/controllers/CountryController.php
@@ -4,16 +4,39 @@ namespace app\controllers;
 use yii\rest\ActiveController;
 
 /**
-     * @SWG\Get(path="/country",
-     *     tags={"country"},
-     *     summary="Lista os países.",
-     *     @SWG\Response(
-     *         response = 200,
-     *         description = "Coleção de países",
-     *         @SWG\Schema(ref = "#/definitions/Country")
-     *     ),
-     * )
-     */
+* @SWG\Get(path="/country",
+*     tags={"country"},
+*     summary="Lista os países.",
+*     @SWG\Response(
+*         response = 200,
+*         description = "Coleção de países",
+*         @SWG\Schema(ref = "#/definitions/Country")
+*     ),
+* )
+* @SWG\Post(path="/country",
+*  tags={"country"},
+*  summary="Cadastra um país",
+*  @SWG\Parameter(
+*      name="Code",
+*      in="formData",
+*      required=true,
+*      type="string",
+*      description="Código do país",
+*  ),
+*  @SWG\Parameter(
+*      name="Name",
+*      in="formData",
+*      required=false,
+*      type="string",
+*      description="Nome do país",
+*  ),
+*  @SWG\Response(
+*      response = 200,
+*      description = "Coleção de países",
+*      @SWG\Schema(ref = "#/definitions/Country")
+*  ),
+* )
+*/
 
 class CountryController extends ActiveController
 {
--- backend/models/Country.php
+++ backend/models/Country.php
@@ -33,28 +33,6 @@ use Yii;
  * @SWG\Property(property="Name", type="string")
  * @SWG\Property(property="Continent", type="string")
  * 
- * @SWG\Post(path="/country",
- *  tags={"country},
- *  summary="Cadastra um país",
- *  @SWG\Parameter(
- *      name="Code",
- *      in="formData",
- *      required=true,
- *      type="string",
- *      description="Código do país",
- *  ),
- *  @SWG\Parameter(
- *      name="Name",
- *      in="formData",
- *      required=false,
- *      type="string",
- *      description="Nome do país",
- *  ),
- *  @SWG\Response(
- *      response = 200,
- *      description = "Coleção de países",
- *      @SWG\Schema(ref = "#/definitions/Country")
- *  ),
  * )
  * 
  */
