9be419a964896aee955bc95882b0a26b20d9a0d3
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2019-10-23 11:28:42 -0300

    documentação

--- backend/controllers/CountryController.php
+++ backend/controllers/CountryController.php
@@ -7,6 +7,20 @@ use yii\rest\ActiveController;
 * @SWG\Get(path="/country",
 *     tags={"country"},
 *     summary="Lista os países.",
+*     @SWG\Parameter(
+*         name="page",
+*         in="query",
+*         required=false,
+*         type="string",
+*         description="Página de exibição",
+*     ),
+*     @SWG\Parameter(
+*         name="per-page",
+*         in="query",
+*         required=false,
+*         type="string",
+*         description="Quantidade de registros por página",
+*     ),
 *     @SWG\Response(
 *         response = 200,
 *         description = "Coleção de países",
@@ -19,6 +33,30 @@ use yii\rest\ActiveController;
 *  @SWG\Parameter(
 *      name="Code",
 *      in="formData",
+*      required=true,
+*      type="string",
+*      description="Código do país",
+*  ),
+*  @SWG\Parameter(
+*      name="Name",
+*      in="formData",
+*      required=false,
+*      type="string",
+*      description="Nome do país",
+*  ),
+*  @SWG\Response(
+*      response = 200,
+*      description = "Coleção de países",
+*      @SWG\Schema(ref = "#/definitions/Country")
+*  ),
+* )
+*
+* @SWG\Put(path="/country/{Code}",
+*  tags={"country"},
+*  summary="Cadastra um país",
+*  @SWG\Parameter(
+*      name="Code",
+*      in="path",
 *      required=true,
 *      type="string",
 *      description="Código do país",
